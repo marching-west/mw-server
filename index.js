eedd3aa1ddf67763baa82ce4ceca24e936bda80a
Josan Coba <josan.coba@gmail.com>
2018-05-08 22:01:29 +0200

    test del sistema de integración continua

---
+++ index.js
@@ -0,0 +1,11 @@
+'use strict'
+const time = require('time')
+
+exports.handler = (event, context, callback) => {
+  const currentTime = new time.Date()
+  currentTime.setTimezone('America/Los_Angeles')
+  callback(null, {
+    statusCode: '200',
+    body: 'The time in Los Angeles is: ' + currentTime.toString()
+  })
+}
